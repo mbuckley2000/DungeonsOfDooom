f8e344fc5c8fb7fd5b804ecc1a316684f62d4f49
mbuckley2000 <mbuckley2000@gmail.com>
2016-04-26 19:40:27 +0100

    Added CGameLogic

---
+++ src/CGameLogic.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Matt Buckley on 26/04/16.
+ */
+public class CGameLogic implements IGameLogic {
+    static {
+        System.loadLibrary("CGameLogic");
+    }
+
+    @Override
+    public native String hello();
+
+    @Override
+    public native synchronized String move(char direction);
+
+    @Override
+    public native String pickup();
+
+    @Override
+    public native String look();
+
+    @Override
+    public native int[] getPlayerPosition();
+
+    @Override
+    public native void setPlayerPosition(int[] playerPosition);
+
+    @Override
+    public native String getLastLookWindow();
+
+    @Override
+    public native int getGoldNeeded();
+
+    @Override
+    public native boolean checkWin();
+}
